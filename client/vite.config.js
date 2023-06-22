4fd7d48de302f7c61e52e503426ad7b676bb364d
adebola-xyz <adebolaakomolafe@gmail.com>
2023-06-22 18:05:43 +0100

    Fix Vite resolution error

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,20 @@
 import { resolve } from "path";
 /**@type {import('vite').UserConfig} */
 export default {
+    root: "src",
+    build: {
+        outDir: resolve(__dirname, "dist"),
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: "src/index.html",
+                questions: "src/questions.html",
+            },
+        },
+    },
     resolve: {
         alias: {
-            "@": resolve(__dirname, "./"),
+            "@": resolve(__dirname, "src/"),
         },
     },
 };
